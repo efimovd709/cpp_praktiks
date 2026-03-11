fd3132d82d18e4c84e6aaff5657e9443272bc020
efimovd709 <efimovd070419@gmail.com>
2026-03-11 13:40:31 +0300

    Update PR_01.cpp

--- 2026_2sem/PR_01/PR_01.cpp
+++ 2026_2sem/PR_01/PR_01.cpp
@@ -1,13 +1,11 @@
-﻿#include <iostream>
+#include <iostream>
 #include <windows.h>
 #include <stdio.h>
 #include "DinoBaraClass.h"
 
 using namespace std;
 
-//глобальную функцию для записи компонентного массива в файл;
 void write2file(DinoBaraClass* dMas) {
-    // stdio.h
     FILE* textFile;
     fopen_s(&textFile, "tetFile.txt", "w+");
     if (textFile) {
@@ -23,208 +21,90 @@ int main()
     setlocale(LC_ALL, "Russian");
     SetConsoleCP(1251);
     SetConsoleOutputCP(1251);
-    cout << "Практическая работа 1\n";
-    //Разработать класс, моделирующий 
-    //динамический массив целых чисел и его обработку
-            //DinoBaraClass* dino0 = new DinoBaraClass();
-            //DinoBaraClass dino1;
-            //DinoBaraClass dino2(5);
-            //dino2.setElement(0, 888);
-            //dino2.display();
-            //DinoBaraClass dino8 = dino2;
-            //dino8.display();
-
-    //DinoBaraClass* d0 = new DinoBaraClass();
-    //d0->addElement(1);            
-    //d0->addElement(9);
-    //d0->addElement(8);
-    //d0->addElement(4);
-    //d0->display();
-    //d0->delElement(3);
-    //d0->display();
-    //d0->delElement(2);
-    //d0->display();
-    //d0->delElement(1);
-    //d0->display();
-    //d0->delElement(0);
-    //d0->display();
-    //cout << endl << "Елемент 0 = " << d0->getElement(0);
-    //d0->setElement(2, 777);
-    //cout << endl << "Елемент 2 = " << d0->getElement(2);
-    //d0->display();
-
-    ////проверка записи в файл
-    //DinoBaraClass* d1 = new DinoBaraClass(33);
-    //write2file(d1);
-
-    ////проверка конструктора копирования
-    //DinoBaraClass* d2 = new DinoBaraClass(23);
-    //DinoBaraClass d3 = *d2;
-    //d3.display();
-
-    //DinoBaraClass* d4 = new DinoBaraClass(5);
-    //d4->display();
-    //d4->insertElement(0, 0);
-    //d4->insertElement(3, 333);
-    //d4->insertElement(5, 666);
-    //d4->display();
-    //cout << endl << "d4->getSize() =  " << d4->getSize();
-    //d4->insertElement(8, 777);
-    //d4->insertElement(9, 888);
-
-    //d4->display();
-
-    // проверка 
-    // перегруженную операцию присваивания operator=();
-    //DinoBaraClass d71(3);
-    //DinoBaraClass d72(4);
-    //cout << "d1";
-    //d71.display();
-    //cout << endl << "d2";
-    //d72.display();
-    //d71 = d72;
-    //cout << endl << "--------- d71 = d72" << endl << "d1";
-    //d71.display();
-    //cout << endl << "d2";
-    //d72.display();
     
-    // проверка 
-    // перегруженную операцию  Вариант 25
-    // 2 ^  Возведение в степень нечетных элементов 
-    // компонентного массива. Показатель степени – правый операнд
-    // 25
-    // 11
-    //DinoBaraClass d88(15);
-    //d88.display();
-    //d88 ^ 3;
-    //d88.display();
-   
-    // 24 
-    //DinoBaraClass d91(3);
-    //d91.display();
-    //DinoBaraClass d92(3);
-    //d92.display();
-    ////DinoBaraClass *d93 = new DinoBaraClass(4);
-    ////d93->display();
-    //cout << "Mega Ira ne s'ela Ezhika = ";
-    //bool b1 = d91 < d92;
-    //cout << b1;
-
-    // 18
-    //DinoBaraClass d991(3);
-    //d991.display();
-    //DinoBaraClass d992(3);
-    //d992.display();
-    //DinoBaraClass d993 = d991 / d992;
-    //d993.display();
-
-    // 23
-    //DinoBaraClass d995(7);
-    //d995.display();
-    //d995.sortDesc();
-    //d995.display();
-
-    // 3
-    //DinoBaraClass d996(7);
-    //d996.display();
-    //DinoBaraClass d997(7);
-    //d997.display();
-    //DinoBaraClass d998 = d996 * d997;
-    //d998.display();
-
-    //16
-    //DinoBaraClass d999(7);
-    //d999.display();
-    //DinoBaraClass d900=!d999;
-    //d900.display();
-    //2
-    //DinoBaraClass d9990(7);
-    //d9990.display();
-    //float _multiplication = *d9990;
-    //cout <<endl<< "Произведение " << _multiplication;
-    //5
-    //DinoBaraClass d2002(7);
-    //d2002.display();
-    //*d2002;
-    //d2002.display();
-    //16 friend
-    //DinoBaraClass d2003(7);
-    //d2003.display();
-    //d2003 / 8;
-    //d2003.display();
-    //2 fiend
-    //DinoBaraClass d2004(7);
-    //d2004.display();
-    //~d2004;
-    //d2004.display();
-
-    // Настя проверка конструктора копирования
-    //DinoBaraClass d10(5);
-    //DinoBaraClass d11=d10;
-    //d10.display();
-    //d11.display();
+    DinoBaraClass array1(6);
+    DinoBaraClass array2(6);
     
-    // Настя 10 вариант
-    //DinoBaraClass d1000(10);
-    //d1000.display();
-    //d1000=d1000 - 200;
-    //d1000.display();
-
-    // Александра 20 Вариант
-    //DinoBaraClass d1001(11);
-    //d1001.display();
-    //!d1001;
-    //d1001.display();
-    // 9
-    // Инкремент элементов компонентного массива с нечетны-ми номерами
-    //DinoBaraClass megaAlisa(7);
-    //megaAlisa.display();
-    //megaAlisa++;
-    //megaAlisa.display();
-    //megaAlisa-500;
-    //megaAlisa.display();
-    //13
-    // Изменение знака элементов компонентного 
-    // массива с нечетными номерами на противоположный
-    //DinoBaraClass megaVarvara(7);
-    //megaVarvara.display();
-    //!megaVarvara;
-    //megaVarvara.display();
-    //megaVarvara*8;
-    //megaVarvara.display();
-
-    // 5
-    //Изменение знака элементов 
-    // компонентного массива на противоположный
-    //DinoBaraClass yarik(3);
-    //yarik.display();
-    //- yarik;
-    //yarik.display();
-
-    // 22
-    //DinoBaraClass megaKate(13);
-    //megaKate.display();
-    //megaKate ^ 7;
-    //megaKate.display();
-    //DinoBaraClass megaGanira1(3);
-    //DinoBaraClass megaGanira2(3);
-    //megaGanira1.display();
-    //megaGanira2.display();
-    //megaGanira1= megaGanira1 * megaGanira2;
-    //megaGanira1.display();
-
-
-    // 5 вариант для Киры с любовью
-    // 1 - Изменение знака элементов компонентного 
-    // массива на противоположный
-    DinoBaraClass megaKira(7);
-    megaKira.display();
-    -megaKira;
-    megaKira.display();
-    // 2 < Сравнение по сумме элементов компонентных массивов
-    DinoBaraClass megaLia(7);
-    bool forwardSasha = megaKira < megaLia;
-    cout << endl << " Lena kupila lambu (Nina tozhe)?  " << forwardSasha;
+    array1.setElement(0, 20);
+    array1.setElement(1, 15);
+    array1.setElement(2, 30);
+    array1.setElement(3, 25);
+    array1.setElement(4, 40);
+    array1.setElement(5, 35);
+    
+    array2.setElement(0, 5);
+    array2.setElement(1, 3);
+    array2.setElement(2, 6);
+    array2.setElement(3, 5);
+    array2.setElement(4, 8);
+    array2.setElement(5, 7);
+    
+    array1.display();
+    array2.display();
+    
+    DinoBaraClass array3 = array1;
+    array3 / array2;
+    array3.display();
+    
+    DinoBaraClass compArray1(4);
+    DinoBaraClass compArray2(4);
+    
+    compArray1.setElement(0, 10);
+    compArray1.setElement(1, 50);
+    compArray1.setElement(2, 30);
+    compArray1.setElement(3, 20);
+    
+    compArray2.setElement(0, 45);
+    compArray2.setElement(1, 25);
+    compArray2.setElement(2, 35);
+    compArray2.setElement(3, 15);
+    
+    compArray1.display();
+    compArray2.display();
+    
+    bool result1 = compArray1 > compArray2;
+    
+    DinoBaraClass compArray3(4);
+    DinoBaraClass compArray4(4);
+    
+    compArray3.setElement(0, 15);
+    compArray3.setElement(1, 25);
+    compArray3.setElement(2, 75);
+    compArray3.setElement(3, 35);
+    
+    compArray4.setElement(0, 80);
+    compArray4.setElement(1, 20);
+    compArray4.setElement(2, 40);
+    compArray4.setElement(3, 60);
+    
+    compArray3.display();
+    compArray4.display();
+    
+    bool result2 = compArray3 > compArray4;
+    
+    DinoBaraClass sortArray(8);
+    sortArray.setElement(0, 45);
+    sortArray.setElement(1, 12);
+    sortArray.setElement(2, 78);
+    sortArray.setElement(3, 23);
+    sortArray.setElement(4, 56);
+    sortArray.setElement(5, 89);
+    sortArray.setElement(6, 34);
+    sortArray.setElement(7, 67);
+    
+    sortArray.display();
+    ~sortArray;
+    sortArray.display();
+    
+    write2file(&sortArray);
+    
+    DinoBaraClass copyArray = sortArray;
+    copyArray.display();
+    
+    DinoBaraClass assignArray;
+    assignArray = sortArray;
+    assignArray.display();
+    
+    return 0;
 }
-
-
+```
