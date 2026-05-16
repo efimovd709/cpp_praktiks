784540ef7d43b9a9432cb24454653519f0a80fb7
efimovd709 <efimovd070419@gmail.com>
2026-05-16 16:59:17 +0300

    Update initial_data.h

--- 2026_2sem/PR_04/initial_data.h
+++ 2026_2sem/PR_04/initial_data.h
@@ -7,37 +7,26 @@ using namespace std;
 
 struct Grade // структура "успеваемость"
 {
-	string subject;		// учебная дисциплина
-	string date;		// оценка
-	int mark;			// дата получения оценки 
+    string subject;     // учебная дисциплина
+    string date;        // дата получения оценки
+    int mark;           // оценка
 };
 
 struct Student // структура "студент"
 {
-	string number;			// номер студ. билета
-	string first_name;		// фамилия студента
-	string birth_day;		// дата рождения студента
-	string group;			// номер группы
-	vector<Grade> results;	// результаты успеваемости
-	float avg_mark;
+    string number;      // номер студ. билета
+    string first_name;  // фамилия студента
+    string birth_day;   // дата рождения студента
+    string group;       // номер группы
+    vector<Grade> results; // результаты успеваемости
+    float avg_mark;
 };
 
 struct Node // структура "узел списка"
 {
-	Student* item;	// данные узла  //сделал указатель чтобы не копировать
-	int index;		// индекс узла
-	Node* next;		// указатель на следующий узел списка
+    Student* item;  // данные узла
+    Node* next;     // указатель на следующий узел списка
 };
-
-//class List	// класс "линейный односвязный список
-//{
-//	Node* first;	// указатель на первый узел списка
-//	Node* last;	// указатель на последний узел списка
-//public:
-//	List();	// прототип конструктора
-//	// методы и функции, реализующие вариативную часть задания
-//};
-
 
 
 
